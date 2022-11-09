9572a9f16de64879539769800de72b97e5068180
teguhprasetyo03 <teguhprasetyo261211@gmail.com>
2022-11-09 22:17:31 +0700

    master branch changed to main

---
+++ app/src/main/java/com/skripsi/aplikasitajwid/hijaiyah/Ba.java
@@ -0,0 +1,16 @@
+package com.skripsi.aplikasitajwid.hijaiyah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.skripsi.aplikasitajwid.R;
+
+public class Ba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ba);
+    }
+}
